0c0482b98907e1344faa0cd29c5ef2b6f015b356
Riya G <rigurn@Riyas-MacBook-Pro.local>
2022-02-28 13:10:39 -0500

    with plots

--- js/barchart.js
+++ js/barchart.js
@@ -17,6 +17,8 @@ const yTooltipOffset = 15;
 
 
 // TODO: What does this code do? 
+  // adds a new svg with given dimensions to build the graph 
+  // within the div with the given id
 const svg1 = d3
   .select("#hard-coded-bar")
   .append("svg")
@@ -41,27 +43,32 @@ const data1 = [
 
 */ 
 
-// TODO: What does this code do? 
+// TODO: What does this code do? - this defines the max y value from data 1
 let maxY1 = d3.max(data1, function(d) { return d.score; });
 
 // TODO: What does each line of this code do?   
+  // defines the range of y values for the graph & where to put 
+  // them in the pixels -- mapping to pixel values
 let yScale1 = d3.scaleLinear()
             .domain([0,maxY1])
             .range([height-margin.bottom,margin.top]); 
 
 // TODO: What does each line of this code do? 
+  // defines the range of x values for the graph & where to put them in the pixels
 let xScale1 = d3.scaleBand()
             .domain(d3.range(data1.length))
             .range([margin.left, width - margin.right])
             .padding(0.1); 
 
 // TODO: What does each line of this code do?  
+  // formats the y-axis of the graph within the svg on the page
 svg1.append("g")
    .attr("transform", `translate(${margin.left}, 0)`) 
    .call(d3.axisLeft(yScale1)) 
    .attr("font-size", '20px'); 
 
 // TODO: What does each line of this code do? 
+  // // formats the x-axis of the graph within the svg
 svg1.append("g")
     .attr("transform", `translate(0,${height - margin.bottom})`) 
     .call(d3.axisBottom(xScale1) 
@@ -75,6 +82,7 @@ svg1.append("g")
 */
 
 // TODO: What does each line of this code do? 
+  // select the given div & append another div for the tooltip
 const tooltip1 = d3.select("#hard-coded-bar") 
                 .append("div") 
                 .attr('id', "tooltip1") 
@@ -82,18 +90,22 @@ const tooltip1 = d3.select("#hard-coded-bar")
                 .attr("class", "tooltip"); 
 
 // TODO: What does each line of this code do?  
+  // defines the text to show up when hovering over a bar in the graph
 const mouseover1 = function(event, d) {
   tooltip1.html("Name: " + d.name + "<br> Score: " + d.score + "<br>") 
           .style("opacity", 1);  
 }
 
 // TODO: What does each line of this code do? 
+  // position the tooltip
 const mousemove1 = function(event, d) {
   tooltip1.style("left", (event.x)+"px") 
           .style("top", (event.y + yTooltipOffset) +"px"); 
 }
 
 // TODO: What does this code do? 
+  // when the mouse in no longer hovering over the graph, the 
+  // tooltip should disappear
 const mouseleave1 = function(event, d) { 
   tooltip1.style("opacity", 0); 
 }
@@ -105,23 +117,105 @@ const mouseleave1 = function(event, d) {
 */
 
 // TODO: What does each line of this code do? 
+  // select bar within svg 
 svg1.selectAll(".bar") 
    .data(data1) 
-   .enter()  
-   .append("rect") 
-     .attr("class", "bar") 
-     .attr("x", (d,i) => xScale1(i)) 
-     .attr("y", (d) => yScale1(d.score)) 
+   .enter()  // makes placeholder for each row
+   .append("rect") // adds rect to each
+     .attr("class", "bar")  // add attribute class, make it bar
+     .attr("x", (d,i) => xScale1(i)) // setting x position for rectangles
+     .attr("y", (d) => yScale1(d.score)) // setting y position for rectangles
      .attr("height", (d) => (height - margin.bottom) - yScale1(d.score)) 
-     .attr("width", xScale1.bandwidth()) 
-     .on("mouseover", mouseover1) 
+     .attr("width", xScale1.bandwidth()) // set height & width for bars
+     .on("mouseover", mouseover1) // add listeners
      .on("mousemove", mousemove1)
      .on("mouseleave", mouseleave1);
 
 
+// ----------------------------------------------------------------------------
 
+/*
+  New code for bar chart from csv
+*/
 
+const svg2 = d3
+  .select("#csv-bar")
+  .append("svg")
+  .attr("width", width-margin.left-margin.right)
+  .attr("height", height - margin.top - margin.bottom)
+  .attr("viewBox", [0, 0, width, height]);
 
+d3.csv("data/barchart.csv").then((data) => {
+  console.log(data);
 
+  let maxY2 = d3.max(data, function(d) { return d.score; });
+
+// TODO: What does each line of this code do?   
+  // defines the range of y values for the graph & where to put 
+  // them in the pixels -- mapping to pixel values
+let yScale2 = d3.scaleLinear()
+            .domain([0,maxY2])
+            .range([height-margin.bottom,margin.top]); 
+
+// TODO: What does each line of this code do? 
+  // defines the range of x values for the graph & where to put them in the pixels
+let xScale2 = d3.scaleBand()
+            .domain(d3.range(data.length))
+            .range([margin.left, width - margin.right])
+            .padding(0.1); 
+
+svg2.append("g")
+   .attr("transform", `translate(${margin.left}, 0)`) 
+   .call(d3.axisLeft(yScale2)) 
+   .attr("font-size", '20px'); 
+
+// TODO: What does each line of this code do? 
+  // // formats the x-axis of the graph within the svg
+svg2.append("g")
+    .attr("transform", `translate(0,${height - margin.bottom})`) 
+    .call(d3.axisBottom(xScale2) 
+            .tickFormat(i => data[i].name))  
+    .attr("font-size", '20px'); 
+
+const tooltip2 = d3.select("#csv-bar") 
+                .append("div") 
+                .attr('id', "tooltip1") 
+                .style("opacity", 0) 
+                .attr("class", "tooltip"); 
+
+// TODO: What does each line of this code do?  
+  // defines the text to show up when hovering over a bar in the graph
+const mouseover2 = function(event, d) {
+  tooltip2.html("Name: " + d.name + "<br> Score: " + d.score + "<br>") 
+          .style("opacity", 1);  
+}
+
+// TODO: What does each line of this code do? 
+  // position the tooltip
+const mousemove2 = function(event, d) {
+  tooltip2.style("left", (event.x)+"px") 
+          .style("top", (event.y + yTooltipOffset) +"px"); 
+}
+
+// TODO: What does this code do? 
+  // when the mouse in no longer hovering over the graph, the 
+  // tooltip should disappear
+const mouseleave2 = function(event, d) { 
+  tooltip2.style("opacity", 0); 
+}
+
+  svg2.selectAll("bar")
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr("class", "bar")
+    .attr("x", (d,i) => xScale2(i)) // setting x position for rectangles
+    .attr("y", (d) => yScale2(d.score)) // setting y position for rectangles
+    .attr("height", (d) => (height - margin.bottom) - yScale2(d.score)) 
+    .attr("width", xScale2.bandwidth()) // set height & width for bars
+    .on("mouseover", mouseover2) // add listeners
+    .on("mousemove", mousemove2)
+    .on("mouseleave", mouseleave2);
+})
 
 
--- js/scatterplot.js
+++ js/scatterplot.js
@@ -8,10 +8,69 @@ Modified: 12/08/21
 
 // Build your scatterplot in this file 
 
+const svgS = d3
+  .select("#csv-scatter")
+  .append("svg")
+  .attr("width", width-margin.left-margin.right)
+  .attr("height", height - margin.top - margin.bottom)
+  .attr("viewBox", [0, 0, width, height]);
 
+d3.csv("data/scatter.csv").then((dataS) => {
+  console.log(dataS);
 
+  let maxY = d3.max(dataS, function(d) { return d.score; });
 
+  let yScale = d3.scaleLinear()
+            .domain([0,maxY])
+            .range([height-margin.bottom,margin.top]); 
 
+  let xScale = d3.scaleBand()
+            .domain(d3.range(dataS.length))
+            .range([margin.left, width - margin.right])
+            .padding(0.1); 
+
+   svgS.append("g")
+   .attr("transform", `translate(${margin.left}, 0)`) 
+   .call(d3.axisLeft(yScale)) 
+   .attr("font-size", '20px'); 
+
+    svgS.append("g")
+    .attr("transform", `translate(0,${height - margin.bottom})`) 
+    .call(d3.axisBottom(xScale) 
+            .tickFormat(i => dataS[i].day))  
+    .attr("font-size", '20px'); 
+
+   const tooltip = d3.select("#csv-scatter") 
+                .append("div") 
+                .attr('id', "tooltip1") 
+                .style("opacity", 0) 
+                .attr("class", "tooltip"); 
+
+  const mouseover = function(event, d) {
+  tooltip.html("Day: " + d.day + "<br> Score: " + d.score + "<br>") 
+          .style("opacity", 1);  
+}
+
+  const mousemove = function(event, d) {
+  tooltip.style("left", (event.x)+"px") 
+          .style("top", (event.y + yTooltipOffset) +"px"); 
+}
+
+   const mouseleave = function(event, d) { 
+  tooltip.style("opacity", 0); 
+}
+
+  svgS.selectAll("circle")
+	.data(dataS)
+	.enter()
+	.append("circle")
+		.attr("cx", (d) => xScale(d.day))
+		.attr("cy", (d) => yScale(d.score))
+		.attr("r", 10)
+		.on("mouseover", mouseover) // add listeners
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave);
+})
 
 
 
